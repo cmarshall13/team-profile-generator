69ce6ad0df7ca046dcbec37f60f4ec6c7a1eb776
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-09-26 14:08:44 -0400

    added get functions and card for manager obj

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const Manager = require('../lib/Manager');
 
 test(`creates Manager object`, () => {
@@ -8,4 +8,30 @@ test(`creates Manager object`, () => {
     expect(manager.id).toBe('28');
     expect(manager.email).toBe('corinnelynette@yahoo.com');
     expect(manager.office).toBe(1313);
+});
+
+test(`get functions return proper values`, () => {
+    const manager = new Manager('Corinne', '28', 'corinnelynette@yahoo.com', 1313);
+
+    expect(manager.getName()).toBe('Corinne');
+    expect(manager.getId()).toBe('28');
+    expect(manager.getEmail()).toBe('corinnelynette@yahoo.com');
+});
+
+test(`getOffice() returns a number`, () => {
+    const manager = new Manager('Corinne', '28', 'corinnelynette@yahoo.com', 1313);
+
+    expect(manager.getOffice()).toEqual(expect.any(Number));
+});
+
+test(`getRole() returns Manager`, () => {
+    const manager = new Manager('Corinne', '28', 'corinnelynette@yahoo.com', 1313);
+
+    expect(manager.getRole()).toBe('Manager');
+});
+
+test(`getCard() returns a string`, () => {
+    const manager = new Manager('Corinne', '28', 'corinnelynette@yahoo.com', 1313);
+
+    expect(manager.getCard()).toEqual(expect.any(String));
 });
