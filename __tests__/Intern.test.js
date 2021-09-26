de25d926a8b8c3cab0cb6563f6b1ba82c22e3ab8
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-09-26 13:51:44 -0400

    added getFunctions and card to intern object

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const Intern = require('../lib/Intern');
 
 test(`create Intern object`, () => {
@@ -8,4 +8,30 @@ test(`create Intern object`, () => {
     expect(intern.id).toBe('28');
     expect(intern.email).toBe('corinnelynette@yahoo.com');
     expect(intern.school).toBe('Western CT State University');
+});
+
+test(`functions to get object descriptions return proper values`, () => {
+    const intern = new Intern('Corinne', '28', 'corinnelynette@yahoo.com', 'Western CT State University');
+
+    expect(intern.getName()).toBe('Corinne');
+    expect(intern.getId()).toBe('28');
+    expect(intern.getEmail()).toBe('corinnelynette@yahoo.com');
+});
+
+test(`getSchool() returns a string`, () => {
+    const intern = new Intern('Corinne', '28', 'corinnelynette@yahoo.com', 'Western CT State University');
+
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+test(`getRole() returns Intern`, () => {
+    const intern = new Intern('Corinne', '28', 'corinnelynette@yahoo.com', 'Western CT State University');
+
+    expect(intern.getRole()).toBe('Intern');
+});
+
+test(`getCard() returns a string`, () => {
+    const intern = new Intern('Corinne', '28', 'corinnelynette@yahoo.com', 'Western CT State University');
+
+    expect(intern.getCard()).toEqual(expect.any(String));
 });
