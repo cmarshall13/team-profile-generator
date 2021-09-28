6aeb14cefba756a5d6a51febfb387ed7fcafab6e
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-09-28 18:58:29 -0400

    creating prompts for user input for manager

--- src/prompts.js
+++ src/prompts.js
@@ -6,3 +6,41 @@ const Intern = require('../lib/Intern');
 const generatePageFunctions = require('./html-output');
 
 
+const teamMembers = [];
+
+module.exports = 
+init = async () => {
+    console.log (
+        'Welcome to your TEAM PROFILE GENERATOR!'
+    )
+    const answers = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: `What is your Manager's name?`,
+                validate: nameInput => {
+                    if (!nameInput) {
+                        console.log(`Please provide the name of your Manager!`);
+                        return false;
+                    }
+                    return true;
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: ({name}) => (`What is ${name}'s' employee ID?`),
+                validate: idInput => {
+                    if(!idInput) {
+                        console.log(`Please enter a valid ID.`);
+                        return false;
+                    }
+                    return true;
+                }
+            },
+            {
+                
+            }
+        ])
+}
