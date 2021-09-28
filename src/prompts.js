da7af7b5a99e4a244e9f27bef758da5ae3a60b57
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-09-28 18:19:13 -0400

    added global const to file and downloaded email-validator

---
+++ src/prompts.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const validator = require('node-email-validation');
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const generatePageFunctions = require('./html-output');
+
+
